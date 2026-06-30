92449af95116c2ffe36e5869bad5030ca97929f2
Omkar Murkute <omkar@example.com>
2026-06-30 19:54:22 +0530

    Add simple edge function test

---
+++ netlify/edge-functions/test/index.ts
@@ -0,0 +1,10 @@
+﻿export const config = {
+  path: ["/api/test-edge"]
+}
+
+export default async (request) => {
+  return new Response(JSON.stringify({ message: 'edge function works' }), {
+    status: 200,
+    headers: { 'Content-Type': 'application/json' },
+  })
+}
