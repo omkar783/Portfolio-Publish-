c11da1dc4168360c3e063af313f499f27f040e49
Omkar Murkute <omkar@example.com>
2026-06-30 18:24:22 +0530

    Trigger redeploy

---
+++ netlify/functions/test.mjs
@@ -0,0 +1,7 @@
+﻿export const handler = async (event) => {
+  return {
+    statusCode: 200,
+    headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
+    body: JSON.stringify({ message: 'hello', path: event.path }),
+  }
+}
